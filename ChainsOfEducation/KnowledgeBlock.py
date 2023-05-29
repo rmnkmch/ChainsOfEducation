1db79b40274879c8add3acacb7cb517570b962af
RomanKaChar <roma-kamch1@mail.ru>
2023-05-29 12:54:15 +0300

    description text splited

--- ChainsOfEducation/KnowledgeBlock.py
+++ ChainsOfEducation/KnowledgeBlock.py
@@ -1,4 +1,10 @@
-import manim
+﻿import manim
+
+
+MIN_DESCRIPTION_FONT_SIZE: float = 10.0
+DEFAULT_DESCRIPTION_FONT_SIZE: float = 30.0
+MIN_TITLE_FONT_SIZE: float = 16.0
+DEFAULT_TITLE_FONT_SIZE: float = 48.0
 
 
 class KnowledgeBlock(manim.RoundedRectangle):
@@ -11,37 +17,77 @@ class KnowledgeBlock(manim.RoundedRectangle):
             **kwargs)
 
         self.title = manim.Text(
-            "Knowledge Block Title",
+            "KnowledgeBlockTitle",
+            font_size=DEFAULT_TITLE_FONT_SIZE,
             weight=manim.BOLD,
             )
         self.set_normal_title_size()
         self.description = manim.Text(
-            "Knowledge Block Description\n1 2 3 4 5 6 7 8 9",
-            font_size=24,
+            '''Матема́тика (др.-греч. μᾰθημᾰτικά[1] < μάθημα «изучение; наука») —
+            точная формальная наука[2], первоначально исследовавшая
+            количественные отношения и пространственные
+            формы[3]. В более современном понимании, это наука об отношениях
+            между объектами, о которых ничего не известно,
+            кроме описывающих их некоторых свойств, — именно тех, которые
+            в качестве аксиом положены в основание той или иной
+            математической теории[4].
+            Математика исторически сложилась на основе операций подсчёта,
+            измерения и описания формы объектов[5]. Математические
+            объекты создаются путём идеализации свойств реальных или
+            других математических объектов и записи этих свойств на
+            формальном языке.
+            Математика не относится к естественным наукам, но широко
+            используется в них как для точной формулировки их содержания,
+            так и для получения новых результатов. Она является
+            фундаментальной наукой, предоставляющей (общие) языковые
+            средства другим наукам; тем самым она выявляет их
+            структурную взаимосвязь и способствует нахождению самых
+            общих законов природы[6].''',
+            font_size=DEFAULT_DESCRIPTION_FONT_SIZE,
             )
-        #self.set_normal_description_size()
+        self.set_normal_description_size()
         self.split_description_lines()
 
         self.add(self.title, self.description)
 
+    def is_unacceptable_width(self, text: manim.Text):
+        return self.width - 0.5 <= text.width
+
     def set_normal_title_size(self):
-        while ((self.title.width > self.width - 0.5) and
-               (self.title.font_size >= 20)):
+        while (self.is_unacceptable_width(self.title) and
+               self.title.font_size > MIN_TITLE_FONT_SIZE):
             self.title.font_size -= 1
         self.title.next_to(self, manim.UP, -self.title.height - 0.25)
 
     def set_normal_description_size(self):
-        while ((self.description.width > self.width - 0.5) and
-               (self.description.font_size >= 14)):
+        while (self.is_unacceptable_width(self.description) and
+               self.description.font_size > MIN_DESCRIPTION_FONT_SIZE):
             self.description.font_size -= 1
         self.description.next_to(self, manim.DOWN, -self.description.height - 0.25)
 
     def split_description_lines(self):
-        print(self.description.text)#already splited?
-        all_text = self.description.text.split()
-        print(all_text)
-        ret_text = ""
-        for str in all_text:
-            ret_text = str + "\n"
-        self.description = manim.Text(ret_text, font_size=24)
+
+        def is_there_another_word():
+            return current_word_index < len(all_text)
+
+        all_text = self.description.original_text.split()
+        splited_text = ""
+        current_word_index = 0
+        while is_there_another_word():
+            current_str = all_text[current_word_index]
+            current_word_index += 1
+            while (is_there_another_word() and
+                   manim.Text(
+                       current_str + " " + all_text[current_word_index],
+                       font_size=self.description.font_size,
+                       ).width < self.width - 0.5):
+                current_str += " " + all_text[current_word_index]
+                current_word_index += 1
+            splited_text += current_str
+            if is_there_another_word():
+                splited_text += "\n"
+
+        self.description = manim.Text(
+            splited_text,
+            font_size=self.description.font_size)
         self.description.next_to(self, manim.DOWN, -self.description.height - 0.25)
