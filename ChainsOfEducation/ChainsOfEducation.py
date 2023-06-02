3e18c385e59ef21d80872a7c827e58f6cdeb6fa6
RomanKaChar <roma-kamch1@mail.ru>
2023-06-02 22:10:40 +0300

    some animations, debug

--- ChainsOfEducation/ChainsOfEducation.py
+++ ChainsOfEducation/ChainsOfEducation.py
@@ -1,13 +1,52 @@
 ﻿import manim
 import KnowledgeBlock
 
+
 class ChainsOfEducation(manim.Scene):
     def construct(self):
         kb = KnowledgeBlock.KnowledgeBlock("MathМатРyaтика!", '''Матема́тика -
-        тоьная наука,ранственньная наука,ьная наука,ранственные формы''')
+        тоьная наука,ранственньная наука,ьная нау
+        ка,ранственные формы''').move_to(3.0 * manim.LEFT).scale(0.3)
+        kb2 = KnowledgeBlock.KnowledgeBlock("Русссска!", '''Текст 
+        (от лат. textus — ткань; сплетение, сочетание) — 
+        зафиксированная на каком-либо материальном носителе 
+        человеческая мысль; в общем плане связная и полная 
+        последовательность символов.
+        Существуют две основные трактовки понятия «текст»: 
+        имманентная (расширенная, философски нагруженная) и 
+        репрезентативная (более частная).''').move_to(manim.RIGHT)
+        kb3 = KnowledgeBlock.KnowledgeBlock("333333тика!", '''3Матема́тика -
+        тоьная нdfdgые формы''').move_to(2.0*manim.RIGHT + 2.0*manim.UP).scale(0.3)
+        kb4 = KnowledgeBlock.KnowledgeBlock("444444М!", '''4Матема́тика -
+        тоьная наукформы''').move_to(2.8*manim.LEFT + 2.0*manim.UP).scale(0.7)
+        kb5 = KnowledgeBlock.KnowledgeBlock("M5555555555а!", '''5М889атема́тика -
+        тоьнdg нау''').move_to(3.0*manim.RIGHT + 3.0*manim.DOWN).scale(0.5)
         self.add(kb)
-        self.play(kb.animate().scale(0.1))
+        self.add(kb2)
         self.wait()
+        self.put_one_kb_into_other(kb, kb2)
+        self.wait()
+        self.add(kb3)
+        self.wait()
+        self.put_one_kb_into_other(kb3, kb2)
+        self.wait()
+        self.add(kb4)
+        self.wait()
+        self.put_one_kb_into_other(kb4, kb2)
+        self.wait()
+        self.add(kb5)
+        self.wait()
+        self.put_one_kb_into_other(kb5, kb2)
+        self.wait()
+
+    def put_one_kb_into_other(self, one: KnowledgeBlock, other: KnowledgeBlock):
+        other.add(one)
+        self.update_subkbs(other)
+
+    def update_subkbs(self, kb: KnowledgeBlock):
+        for subkb_info in kb.get_subkb_info_to_update():
+            self.play(subkb_info[0].animate.scale(
+                subkb_info[1]).move_to(subkb_info[2]))
 
 #cd /d D:\My\LTTDIT\Python\ChainsOfEducation\ChainsOfEducation
 #manim -pql ChainsOfEducation.py ChainsOfEducation
--- ChainsOfEducation/KnowledgeBlock.py
+++ ChainsOfEducation/KnowledgeBlock.py
@@ -1,16 +1,21 @@
 ﻿import manim
 
 
+DEFAULT_HEIGHT: float = 4.5
+DEFAULT_WIDTH: float = 8.0
+
 MIN_DESCRIPTION_FONT_SIZE: float = 10.5
-DEFAULT_DESCRIPTION_FONT_SIZE: float = 32.0
+DEFAULT_DESCRIPTION_FONT_SIZE: float = 12.0#32.0
 MIN_TITLE_FONT_SIZE: float = 18.5
 DEFAULT_TITLE_FONT_SIZE: float = 56.0
 
 DEFAULT_PADDING: float = 0.3
+DEFAULT_UNDERLINE_TITLE_OFFSET: float = 0.3
 
 
 class KnowledgeBlock(manim.RoundedRectangle):
-    """Base class for all blocks of knowledge"""
+    """Base class for all blocks of knowledge
+    kb - KnowledgeBlock"""
 
     def __init__(
         self,
@@ -18,8 +23,8 @@ class KnowledgeBlock(manim.RoundedRectangle):
         description: str = "KnowledgeBlockDescription",
         **kwargs):
         super().__init__(
-            height=4.5,
-            width=8.0,
+            height=DEFAULT_HEIGHT,
+            width=DEFAULT_WIDTH,
             **kwargs)
 
         self.title = manim.Text(
@@ -40,19 +45,77 @@ class KnowledgeBlock(manim.RoundedRectangle):
 
     def scale(self, scale_factor: float, **kwargs):
         super().scale(scale_factor, **kwargs)
+        return self
 
-    def is_acceptable_width(self, text: manim.Text):
-        return text.width < self.width - 2.0 * DEFAULT_PADDING
+    def add(self, *mobjects):
+        super().add(*mobjects)
+        return self
 
-    def is_acceptable_height(self, text: manim.Text):
+    def get_subkb_info_to_update(self):
+        ret_info = []
+        current_kb_index = 0
+        for kb in self.get_all_subkbs():
+            scale = self.get_subkb_scale(current_kb_index)
+            pos = self.get_subkb_pos(current_kb_index)
+            ret_info.append(tuple([kb, scale, pos]))
+            current_kb_index += 1
+        return ret_info
+
+    def get_subkb_scale(self, index: int):
+        return 1.0
+        subkb_scale: float = 0.5
+        subkbs = self.get_all_subkbs()
+        if (index == 0) and (len(subkbs) == 1):
+            subkb_scale = 1.0
+        return subkb_scale * 0.5 * self.width / subkbs[index].width
+
+    def get_subkb_pos(self, index: int):
+        return self.get_center()
+        vertical_space = (self.height - self.title.height
+                          - (DEFAULT_UNDERLINE_TITLE_OFFSET
+                             + 2.0 * DEFAULT_PADDING)
+                          * self.get_kb_proportion())
+        subkb_pos = (manim.RIGHT * 0.25 * self.width
+                     + manim.DOWN * (0.5 * self.height
+                                     - 0.5 * vertical_space
+                                     - DEFAULT_PADDING * self.get_kb_proportion())
+                     + self.get_center())
+        horizontal_dop_part = manim.LEFT * 0.125 * self.width
+        vertical_dop_part = 0.25 * manim.UP * vertical_space
+        if (index == 0) and not (len(self.get_all_subkbs()) == 1):
+            subkb_pos += horizontal_dop_part + vertical_dop_part
+        elif index == 1:
+            subkb_pos += - horizontal_dop_part + vertical_dop_part
+        elif index == 2:
+            subkb_pos += horizontal_dop_part - vertical_dop_part
+        else:
+            subkb_pos += - horizontal_dop_part - vertical_dop_part
+        return subkb_pos
+
+    def get_all_subkbs(self):
+        return [kb for kb in self.submobjects if isinstance(kb, KnowledgeBlock)]
+
+    def get_kb_proportion(self):
+        return self.width / DEFAULT_WIDTH
+
+    def is_acceptable_title_width(self):
+        return self.title.width < self.width - 2.0 * DEFAULT_PADDING
+
+    def is_acceptable_description_width(self, descr: manim.Text):
+        if len(self.get_all_subkbs()) >= 1:
+            return descr.width < 0.5 * self.width - 2.0 * DEFAULT_PADDING
+        return descr.width < self.width - 2.0 * DEFAULT_PADDING
+
+    def is_acceptable_description_height(self, text: manim.Text):
         return (text.height < self.height - self.title.height
-                - 2.0 * DEFAULT_PADDING - 0.3)
+                - (2.0 * DEFAULT_PADDING + DEFAULT_UNDERLINE_TITLE_OFFSET)
+                * self.get_kb_proportion())
 
     def set_normal_title_size(self):
-        while (not self.is_acceptable_width(self.title) and
+        while (not self.is_acceptable_title_width() and
                self.title.font_size > MIN_TITLE_FONT_SIZE):
             self.title.font_size -= 1.0
-        self.title.next_to(self, manim.UP, -self.title.height - DEFAULT_PADDING)
+        self.title.next_to(self, manim.UP, - self.title.height - DEFAULT_PADDING)
 
     def build_description(self):
         self.set_normal_description_size()
@@ -60,16 +123,19 @@ class KnowledgeBlock(manim.RoundedRectangle):
 
     def move_description_in_correct_position(self):
         self.description.next_to(self, manim.DOWN,
-                                 - 0.5 * (self.height - self.title.height - 0.3)
+                                 - 0.5 * (self.height - self.title.height
+                                          - DEFAULT_UNDERLINE_TITLE_OFFSET)
                                  - 0.5 * self.description.height)
+        if len(self.get_all_subkbs()) >= 1:
+            self.description.shift(manim.LEFT * 0.5 * self.width)
 
     def set_normal_description_size(self):
         self.split_description_lines()
-        while (not self.is_acceptable_height(self.description) and
+        while (not self.is_acceptable_description_height(self.description) and
                self.description.font_size > MIN_DESCRIPTION_FONT_SIZE):
             self.description.font_size -= 1.0
             self.split_description_lines()
-        while (not self.is_acceptable_width(self.description) and
+        while (not self.is_acceptable_description_width(self.description) and
                self.description.font_size > MIN_DESCRIPTION_FONT_SIZE):
             self.description.font_size -= 1.0
 
@@ -85,7 +151,7 @@ class KnowledgeBlock(manim.RoundedRectangle):
             current_str: str = all_text[current_word_index]
             current_word_index += 1
             while (is_there_another_word() and
-                   self.is_acceptable_width(
+                   self.is_acceptable_description_width(
                        manim.Text(
                            current_str + " " + all_text[current_word_index],
                            font_size=self.description.font_size))):
