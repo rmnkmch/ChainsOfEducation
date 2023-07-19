cb9d24d105f80f90a7490ceebbfbf2006ec54a50
RomanKaChar <roma-kamch1@mail.ru>
2023-07-19 21:15:12 +0300

    51%

--- ChainsOfEducation/ChainsOfEducation.py
+++ ChainsOfEducation/ChainsOfEducation.py
@@ -1,4 +1,4 @@
-﻿import manim
+﻿import manim as M
 import Block
 import KnowledgeBlock as KB
 import random
@@ -11,9 +11,9 @@ import Chain
 FAST_RUN_TIME: float = 0.1
 
 
-class ChainsOfEducation(manim.Scene):
+class ChainsOfEducation(M.Scene):
     def construct(self):
-        self.chapter_1_0()
+        self.chapter_1_1()
 
     def load_all(self):
         self.sql_db = SQLDatabase.SQLDatabase()
@@ -90,11 +90,11 @@ class ChainsOfEducation(manim.Scene):
                 self.wait()
         else:
             if fast:
-                self.play(manim.AnimationGroup(
+                self.play(M.AnimationGroup(
                     b.get_animations_to_play(), other_animations,
                     run_time = FAST_RUN_TIME))
             else:
-                self.play(manim.AnimationGroup(
+                self.play(M.AnimationGroup(
                     b.get_animations_to_play(), other_animations))
                 self.wait()
 
@@ -112,9 +112,9 @@ class ChainsOfEducation(manim.Scene):
         my_pos_x = (random.random() - 0.5) * 6.0
         my_pos_y = (random.random() - 0.5) * 3.0
         if anim:
-            b.target.move_to(my_pos_x * manim.RIGHT + my_pos_y * manim.UP)
+            b.target.move_to(my_pos_x * M.RIGHT + my_pos_y * M.UP)
         else:
-            b.move_to_outside(my_pos_x * manim.RIGHT + my_pos_y * manim.UP)
+            b.move_to_outside(my_pos_x * M.RIGHT + my_pos_y * M.UP)
 
     def scale_random(self, b: Block.Block, anim = True):
         my_scl = 1.0 + (random.random() - 0.5) * 0.6
@@ -126,13 +126,13 @@ class ChainsOfEducation(manim.Scene):
     def wait(self, duration: float = 0.2, **kwargs):
         super().wait(duration, **kwargs)
 
-    def add(self, *mobjects: manim.Mobject):
+    def add(self, *mobjects: M.Mobject):
         super().add(*mobjects)
         for b in mobjects:
             if isinstance(b, KB.KnowledgeBlock):
                 self.add(b.containing_b, b.description, b.ellipsis_b)
 
-    def remove(self, *mobjects: manim.Mobject):
+    def remove(self, *mobjects: M.Mobject):
         super().remove(*mobjects)
         for b in mobjects:
             if isinstance(b, KB.KnowledgeBlock):
@@ -142,54 +142,54 @@ class ChainsOfEducation(manim.Scene):
         kb.save_description_opacity()
         kb.hide_description(False)
         if fast:
-            self.play(manim.Create(kb, run_time = FAST_RUN_TIME))
+            self.play(M.Create(kb, run_time = FAST_RUN_TIME))
         else:
-            self.play(manim.Create(kb, lag_ratio = 0.1, run_time = 3.0))
+            self.play(M.Create(kb, lag_ratio = 0.1, run_time = 3.0))
             self.wait()
 
     def write_text(self, text, fast = False):
         if fast:
-            self.play(manim.Write(text, run_time = FAST_RUN_TIME))
+            self.play(M.Write(text, run_time = FAST_RUN_TIME))
         else:
-            self.play(manim.Write(text, run_time = 3.0))
+            self.play(M.Write(text, run_time = 3.0))
             self.wait()
 
     def unwrite_text(self, text, fast = False):
         if fast:
-            self.play(manim.Unwrite(text, run_time = FAST_RUN_TIME,
+            self.play(M.Unwrite(text, run_time = FAST_RUN_TIME,
                                     reverse = False))
         else:
-            self.play(manim.Unwrite(text, run_time = 3.0, reverse = False))
+            self.play(M.Unwrite(text, run_time = 3.0, reverse = False))
             self.wait()
 
     def creating_topic_anim(self, topic: TopicBlock.TopicBlock, fast = False):
         if fast:
-            return manim.Create(topic, run_time = FAST_RUN_TIME)
+            return M.Create(topic, run_time = FAST_RUN_TIME)
         else:
-            return manim.Create(topic, lag_ratio = 0.1)
+            return M.Create(topic, lag_ratio = 0.1)
 
     def creating_chain_anim(self, chain: Chain.Chain, fast = False):
         if fast:
-            return manim.AnimationGroup(
-                manim.FadeIn(chain.start_dot),
-                manim.Write(chain),
-                manim.Create(chain.end_dot),
+            return M.AnimationGroup(
+                M.FadeIn(chain.start_dot),
+                M.Write(chain),
+                M.Create(chain.end_dot),
                 run_time = FAST_RUN_TIME)
         else:
-            return manim.AnimationGroup(
-                manim.FadeIn(chain.start_dot),
-                manim.Write(chain),
-                manim.Create(chain.end_dot),
+            return M.AnimationGroup(
+                M.FadeIn(chain.start_dot),
+                M.Write(chain),
+                M.Create(chain.end_dot),
                 lag_ratio = 0.75)
 
     def creating_chain_and_topic_anim(self, chain, topic, fast = False):
         if fast:
-            return manim.AnimationGroup(
+            return M.AnimationGroup(
                 self.creating_chain_anim(chain, fast),
                 self.creating_topic_anim(topic, fast),
                 run_time = FAST_RUN_TIME)
         else:
-            return manim.AnimationGroup(
+            return M.AnimationGroup(
                 self.creating_chain_anim(chain, fast),
                 self.creating_topic_anim(topic, fast),
                 lag_ratio = 0.5)
@@ -207,12 +207,12 @@ manim -pqh ChainsOfEducation.py ChainsOfEducation
         def get_start_chain_func():
             n = self.nnn
             self.nnn += 1
-            return (lambda: manim.UP * y_values[n + 1]
-                    + manim.RIGHT * x_values[n + 1])
+            return (lambda: M.UP * y_values[n + 1]
+                    + M.RIGHT * x_values[n + 1])
 
         fast_1 = True
         fast_2 = False
-        """intro_text = manim.Text("Ну что ж ...")
+        intro_text = M.Text("Ну что ж ...")
         self.write_text(intro_text, fast_1)
         self.unwrite_text(intro_text, fast_1)
         x_values = [-7, -4, 0, 4, 7]
@@ -221,18 +221,18 @@ manim -pqh ChainsOfEducation.py ChainsOfEducation
         arrow = ComplexArrow.ComplexArrow(coords)
         anim_1 = ChainsOfEducation.MyMoveAlongPath(
             arrow.end_tip, arrow.copy(), run_time = 1.0)
-        anim_2 = manim.Create(arrow, run_time = 1.0)
-        anim_3 = manim.AnimationGroup(anim_1, anim_2)
+        anim_2 = M.Create(arrow, run_time = 1.0)
+        anim_3 = M.AnimationGroup(anim_1, anim_2)
         tb_1 = TopicBlock.TopicBlock(
             "Осознание", ["Что такое осознанность?",
                           "Несколько\nуниверсальных методов"]).move_to(
-            1.9 * manim.UP + 4.0 * manim.LEFT).scale(0.3)
+            1.9 * M.UP + 4.0 * M.LEFT).scale(0.3)
         tb_1.set_chain(get_start_chain_func())
         tb_2 = TopicBlock.TopicBlock("???").move_to(
-            1.9 * manim.UP + 0.0 * manim.LEFT).scale(0.3)
+            1.9 * M.UP + 0.0 * M.LEFT).scale(0.3)
         tb_2.set_chain(get_start_chain_func())
         tb_3 = TopicBlock.TopicBlock("???").move_to(
-            1.9 * manim.UP + 4.0 * manim.RIGHT).scale(0.3)
+            1.9 * M.UP + 4.0 * M.RIGHT).scale(0.3)
         tb_3.set_chain(get_start_chain_func())
         grp = [tb_1, tb_2, tb_3]
         anims = []
@@ -240,61 +240,65 @@ manim -pqh ChainsOfEducation.py ChainsOfEducation
             anims.append(self.creating_chain_and_topic_anim(
                 topic_block.chain, topic_block, fast_1))
             topic_block.chain.stop_follow()
-        anims = manim.AnimationGroup(*anims, lag_ratio = 0.1)
-        played = manim.AnimationGroup(anim_3, anims, lag_ratio = 0.3)
+        anims = M.AnimationGroup(*anims, lag_ratio = 0.1)
+        played = M.AnimationGroup(anim_3, anims, lag_ratio = 0.3)
         self.play(played)
         for topic_block in grp:
             topic_block.chain.start_follow()
         tb_1.generate_target()
-        tb_1.target.move_to(1.5 * manim.DOWN + 3.0 * manim.LEFT).scale(1.0 / 0.3)
+        tb_1.target.move_to(1.5 * M.DOWN + 3.0 * M.LEFT).scale(1.0 / 0.3)
         tb_1.target.activate()
-        self.play(manim.MoveToTarget(tb_1))
+        self.play(M.MoveToTarget(tb_1))
         self.play(tb_1.prepare_to_briefs())
         for _ in range(len(tb_1.get_all_briefs_dots())):
-            self.play(tb_1.show_next_brief())"""
+            self.play(tb_1.show_next_brief())
 
         kb_1 = KB.KnowledgeBlock("Осознанность!",
         '''Очень сильный и важный инструмент.''')
         self.create_kb_no_descr(kb_1, fast_1)
         self.update_b(kb_1, fast = fast_1)
         kb_1.generate_target()
-        kb_1.target.move_to(manim.UP * 2.2).scale(0.5)
+        kb_1.target.move_to(M.UP * 2.2).scale(0.5)
         self.update_b(kb_1, False, fast_1)
         x_values_2 = [0, 0]
         y_values_2 = [0.8, -1.0]
         coords_2 = [(x, y, 0.0) for x, y in zip(x_values_2, y_values_2)]
         arrow_2 = ComplexArrow.ComplexArrow(coords_2)
-        arrow_2.end_tip.rotate(- 0.5 * manim.PI)
+        arrow_2.end_tip.rotate(- 0.5 * M.PI)
         anim_10 = ChainsOfEducation.MyMoveAlongPath(
             arrow_2.end_tip, arrow_2.copy())
-        anim_11 = manim.Create(arrow_2)
-        text_1 = manim.Text(
+        anim_11 = M.Create(arrow_2)
+        text_1 = M.Text(
         "Осознавая свои принятые решения,\nжелаемое будет достигаться быстрее.",
-        font_size = 30).move_to(2.0 * manim.DOWN)
-        anim_12 = manim.AddTextLetterByLetter(text_1, time_per_char = 0.03)
-        anim_13 = manim.AnimationGroup(anim_11, anim_10)
-        anim_14 = manim.AnimationGroup(anim_13, anim_12, lag_ratio = 0.5)
+        font_size = 30).move_to(2.0 * M.DOWN)
+        anim_12 = M.AddTextLetterByLetter(text_1, time_per_char = 0.01)
+        anim_13 = M.AnimationGroup(anim_11, anim_10)
+        anim_14 = M.AnimationGroup(anim_13, anim_12, lag_ratio = 0.5)
         self.play(anim_14)
-
-       
-
+        vgrp_1 = M.VGroup(arrow_2, arrow_2.end_tip, text_1)
+        vgrp_1.generate_target()
+        vgrp_1.target.shift(14.0 * M.LEFT)
+        kb_1.generate_target()
+        kb_1.target.shift(14.0 * M.LEFT)
+        kb_1.make_finish_target()
+        self.play(M.MoveToTarget(vgrp_1), kb_1.get_animations_to_play())
         self.wait(1.0)
 
-    def chapter_1_2(self):
-        text_2 = manim.Text("""Любые суждения или мысли кажутся непонятными
+    def chapter_1_1(self):
+        text_2 = M.Text("""Любые суждения или мысли кажутся непонятными
         и автоматически бессмысленными, если у нас нет
         достаточного основания для их понимания.""")
-        pass
-
+        for word in text_2.original_text.split():
+            self.play(M.FadeIn(M.Text(word)))
 
     def test_1(self):
         self.wait(1.0)
 
-    class MyMoveAlongPath(manim.Animation):
+    class MyMoveAlongPath(M.Animation):
         def __init__(
             self,
-            mobject: manim.Mobject,
-            path: manim.VMobject,
+            mobject: M.Mobject,
+            path: M.VMobject,
             suspend_mobject_updating: bool | None = False,
             **kwargs) -> None:
             self.path = path
@@ -316,11 +320,11 @@ manim -pqh ChainsOfEducation.py ChainsOfEducation
                 if diff_pos[0] > 0.0 and diff_pos[1] > 0.0:
                     angle = atan(diff_pos[1] / diff_pos[0])
                 elif diff_pos[0] > 0.0 and diff_pos[1] < 0.0:
-                    angle = 2.0 * manim.PI - atan(abs(diff_pos[1] / diff_pos[0]))
+                    angle = 2.0 * M.PI - atan(abs(diff_pos[1] / diff_pos[0]))
                 elif diff_pos[0] < 0.0 and diff_pos[1] > 0.0:
-                    angle = manim.PI - atan(abs(diff_pos[1] / diff_pos[0]))
+                    angle = M.PI - atan(abs(diff_pos[1] / diff_pos[0]))
                 else:
-                    angle = manim.PI + atan(abs(diff_pos[1] / diff_pos[0]))
+                    angle = M.PI + atan(abs(diff_pos[1] / diff_pos[0]))
                 diff_angle = angle - self.previous_angle
                 self.previous_angle = angle
                 self.mobject.rotate(diff_angle)
