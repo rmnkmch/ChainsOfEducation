3975ef1dcc23096695858d0681f535ccddb0ecc5
RomanKaChar <roma-kamch1@mail.ru>
2023-08-02 21:48:57 +0300

    arrow chains text block 50%

--- ChainsOfEducation/TextBlock.py
+++ ChainsOfEducation/TextBlock.py
@@ -1,5 +1,18 @@
-import math
 import manim
+import enum
+
+
+class Directions(enum.Enum):
+    UP = "UP",
+    DOWN = "DOWN",
+    RIGHT = "RIGHT",
+    LEFT = "LEFT",
+    IN = "IN",
+    OUT = "OUT",
+    UL = "UL",
+    UR = "UR",
+    DL = "DL",
+    DR = "DR",
 
 
 class TextBlock(manim.Text):
@@ -11,7 +24,7 @@ class TextBlock(manim.Text):
         fill_opacity: float  = 0.5,
         stroke_width: float = 0.0,
         color: str = "#FFFFFF",
-        font_size: float = 48.0,
+        font_size: float = 30.0,
         line_spacing: float = - 1.0,
         font: str = "",
         slant: str = "NORMAL",
@@ -47,36 +60,58 @@ class TextBlock(manim.Text):
         from math import sqrt
         return sqrt(vector[0] * vector[0] + vector[1] * vector[1])
 
-    def get_arrow_point(self, direction = 0.0):
-        return self.background_rectangle.point_from_proportion(direction)
+    def pp2direction(self):
+        pass
+
+    def direction2pp(self, direction: Directions):
+        match direction:
+            case Directions.UP:
+                return 0.5 * self.init_width / self.perimeter()
+            case Directions.DOWN:
+                return ((1.5 * self.init_width + self.init_height)
+                        / self.perimeter())
+            case Directions.RIGHT:
+                return ((2.0 * self.init_width + 1.5 * self.init_height)
+                        / self.perimeter())
+            case Directions.LEFT:
+                return (self.init_width + 0.5 * self.init_height) / self.perimeter()
+            case Directions.UL:
+                return self.init_width / self.perimeter()
+            case Directions.UR:
+                return 0.0
+            case Directions.DL:
+                return 0.5
+            case Directions.DR:
+                return (2.0 * self.init_width + self.init_height) / self.perimeter()
+            case _:
+                return 0.0
+
+    def get_arrow_point(self, pp = 0.0):
+        return self.background_rectangle.point_from_proportion(pp)
 
     def get_arrright(self):
-        return self.get_arrow_point(
-            (2.0 * self.init_width + 1.5 * self.init_height) / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.RIGHT))
 
     def get_arrup(self):
-        return self.get_arrow_point(0.5 * self.init_width / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.UP))
 
     def get_arrleft(self):
-        return self.get_arrow_point(
-            (self.init_width + 0.5 * self.init_height) / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.LEFT))
 
     def get_arrdown(self):
-        return self.get_arrow_point(
-            (1.5 * self.init_width + self.init_height) / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.DOWN))
 
     def get_arrur(self):
-        return self.get_arrow_point(0.0)
+        return self.get_arrow_point(self.direction2pp(Directions.UR))
 
     def get_arrul(self):
-        return self.get_arrow_point(self.init_width / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.UL))
 
     def get_arrdl(self):
-        return self.get_arrow_point(0.5)
+        return self.get_arrow_point(self.direction2pp(Directions.DL))
 
     def get_arrdr(self):
-        return self.get_arrow_point(
-            (2.0 * self.init_width + self.init_height) / self.perimeter())
+        return self.get_arrow_point(self.direction2pp(Directions.DR))
 
     def get_buff_arrow_point(self, direction, buff = 0.5):
         vect = direction - self.get_center()
@@ -93,3 +128,53 @@ class TextBlock(manim.Text):
 
     def get_buff_arrdown(self, buff = 0.5):
         return self.get_buff_arrow_point(self.get_arrdown(), buff)
+
+    def get_smooth_arrow_side(
+        self, direction, buff = 1.0, outer_buff_num = 1, inner_buff_num = 20):
+        from math import floor
+        opposite = direction + 0.5
+        if opposite > 1.0: opposite -= 1.0
+        points: list = []
+        dops = (self.vector_len(
+            self.get_arrow_point(direction) - self.get_center()) / buff)
+        dops = min(floor(dops), inner_buff_num)
+        for i in range(outer_buff_num, -dops, -1):
+            points.append(self.get_buff_arrow_point(
+                self.get_arrow_point(direction), buff * i))
+        points.append(self.get_center())
+        for i in range(-dops + 1, outer_buff_num + 1, 1):
+            points.append(self.get_buff_arrow_point(
+                self.get_arrow_point(opposite), buff * i))
+        return points
+
+    def get_smooth_arrright(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.RIGHT), buff)
+
+    def get_smooth_arrup(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.UP), buff)
+
+    def get_smooth_arrleft(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.LEFT), buff)
+
+    def get_smooth_arrdown(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.DOWN), buff)
+
+    def get_smooth_arrur(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.UR), buff)
+
+    def get_smooth_arrul(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.UL), buff)
+
+    def get_smooth_arrdl(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.DL), buff)
+
+    def get_smooth_arrdr(self, buff = 1.0):
+        return self.get_smooth_arrow_side(
+            self.direction2pp(Directions.DR), buff)
